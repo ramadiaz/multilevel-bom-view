b4fc73a3e68da808b7d6a064b03ef01c0a20e5e9
Rama Diaz <ramadiaz221@gmail.com>
2024-03-18 14:34:29 +0700

    Major update

--- app/Views/api_data_view.php
+++ app/Views/api_data_view.php
@@ -7,54 +7,96 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>API Data</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/phosphor-icons@1.4.2/src/index.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/phosphor-icons@1.4.2/src/index.min.js"></script>
     <script>
         function toggleList(list) {
             list.classList.toggle('hidden');
+        }
+
+        function collapseAll() {
+            const nestedLists = document.querySelectorAll('.nested-list');
+            nestedLists.forEach(list => {
+                list.classList.add('hidden');
+            });
         }
     </script>
 </head>
 
 <body>
-    <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+    <!-- <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
         <span class="sr-only">Open sidebar</span>
         <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
         </svg>
-    </button>
+    </button> -->
 
-    <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-        <div class="h-full overflow-y-auto bg-gray-50 dark:bg-gray-800">
-            <ul class="space-y-2 font-medium">
-                <li>
-                    <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <span class="ms-3">BOM</span>
-                    </a>
-                </li>
-            </ul>
+    <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 border-r border-neutral-600" aria-label="Sidebar">
+        <div class="h-full overflow-y-auto bg-neutral-950 text-slate-200  ">
+            <div class="font-normal flex flex-col">
+                <div class="px-4 py-4 font-semibold bg-gradient-to-br from-blue-600 to-green-400 ">
+                    <img src="/logo.png" alt="logo">
+                </div>
+                <a class="px-4 py-4 hover:bg-gradient-to-br hover:from-green-400 hover:to-blue-600 from-30% transition-all duration-500" href="/">Bill of Materials</a>
+                
+            </div>
         </div>
     </aside>
 
-    <div class="p-4 sm:ml-64">
+    <div class="bg-neutral-900 text-slate-200 min-h-screen py-4 text-sm sm:ml-64">
+        <button onclick="collapseAll()" class="relative inline-flex items-center justify-center p-0.5 mx-4 my-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 text-white focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 transition-all duration-500">
+            <span class="relative px-4 py-1 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0 text-sm transition-all duration-500">
+                Collapse all
+            </span>
+        </button>
         <?php
         function displayItemsRecursive($json_data, $parent_id, $level = 0)
         {
             $filteredItems = array_filter($json_data, function ($item) use ($parent_id) {
                 return $item['parent_id'] === $parent_id;
-            }); ?>
+            });
+        ?>
 
-            <ul>
-
+            <ul class='font-mono'>
                 <?php
-                foreach ($filteredItems as $item) { ?>
-                    <li  class="border border-slate-700 select-none">
-                        <button type="button" onclick="toggleList(this.nextElementSibling)">
-                            <?php echo str_repeat('`-', $level);?>
-                            <?= $item['component_id'] ?> ||
-                            <?= $item['net'] ?>
-                            <?= $item['component_inv'] ?>
-                            <span class="float-right">[+]</span>
+                foreach ($filteredItems as $item) {
+                    $childItemsCount = count(array_filter($json_data, function ($childItem) use ($item) {
+                        return $childItem['parent_id'] === $item['component_id'];
+                    }));
+                ?>
+                    <li class="uppercase select-none bg-neutral-800">
+                        <button type="button" onclick="toggleList(this.nextElementSibling)" class="flex flex-row items-center gap-3 px-4 py-2 whitespace-none">
+                            <?php if (hasChildItems($json_data, $item['component_id'])) { ?>
+                                <i class="ph ph-arrow-elbow-down-right"></i>
+                            <?php } else { ?>
+                                <i class='w-[14px]'></i>
+                            <?php } ?>
+                            <h6 class="inline-flex items-center gap-1"><?= $childItemsCount ?> <i class="ph ph-cube"></i> </h6>
+
+                            <span class="opacity-75 ">
+                                id:
+                            </span>
+                            <?= $item['component_id'] ?>
+                            <?php if (isset($item['component_desc'])) { ?>
+                                <span class="opacity-75 ">
+                                    desc:
+                                </span>
+                                <?= $item['component_desc'] ?>
+                            <?php } ?>
+                            <?php if (isset($item['net']) && $item['net'] !== 0) { ?>
+                                <span class="opacity-75 ">
+                                    net:
+                                </span>
+                                <?= $item['net'] ?>
+                            <?php } ?>
+                            <?php if (isset($item['component_inv'])) { ?>
+                                <span class="opacity-75 ">
+                                    inv:
+                                </span>
+                                <?= $item['component_inv'] ?>
+                            <?php } ?>
                         </button>
-                        <ul class="hidden pl-4 list-disc list-inside">
+                        <ul class="ml-6 nested-list hidden list-disc list-inside transition-all duration-300">
                             <?php
                             displayItemsRecursive($json_data, $item['component_id'], $level + 1); ?>
                         </ul>
@@ -64,9 +106,20 @@
         <?php } ?>
 
         <?php
-        displayItemsRecursive($json_data, 'F-ACDP-105D31L-B8C3-AP-PRA0');
+        function hasChildItems($json_data, $parent_id)
+        {
+            foreach ($json_data as $item) {
+                if ($item['parent_id'] === $parent_id) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        displayItemsRecursive($json_data, 'PARENT');
         ?>
     </div>
+
 </body>
 
 </html>
